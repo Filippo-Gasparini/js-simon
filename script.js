271ed82734ddb226a531aa85cac897d62a7bfee5
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-24 16:09:02 +0200

    ADD: correzione

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Simon Says')
+console.log('js-simon')
+
+// vado a inserire il countdown nel layout con la funzione
+
+
+//message.addEventListener('click', function(){
+  //  setTimeout('click', 3000);
+    //console.log(message);
+//})
+
+
+//const message = setInterval(function() {
+  //  let distance = countDownDate - now;
+//})
+
+
+//setTimeout = ('genera' 300000);
+
+
+
+
+//const interval = setInterval(function(){
+  //  var d = new Date();
+    //document.getElementById('cou').innerHTML = d;
+   // }, 30000);
+
+//document.getElementById('btn-stop').onclick = function(){
+
+  //      clearInterval(interval);
+//};
+
+
